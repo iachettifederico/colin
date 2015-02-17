2d9d306afea81aeaf66a59211845c7f8177e5f19
Federico Iachetti <iachetti.federico@gmail.com>
2015-02-16 21:12:34 -0300

    Wrapped parser inside a module

--- lib/colin.rb
+++ lib/colin.rb
@@ -1,79 +1,81 @@
-class Colin
-  def initialize(args=[])
-    @args = args
-    @options = {}
-    @skipped = []
-    parse
-  end
+module Colin
+  class Parser
+    def initialize(args=[])
+      @args = args
+      @options = {}
+      @skipped = []
+      parse
+    end
 
-  def options
-    @options
-  end
+    def options
+      @options
+    end
 
-  def args
-    @skipped
-  end
+    def args
+      @skipped
+    end
 
-  private
+    private
 
-  def parse
-    (@args + [nil]).each do |opt|
-      case opt
-      when /^\w+$/
-        if @current
-          set(@current, opt)
+    def parse
+      (@args + [nil]).each do |opt|
+        case opt
+        when /^\w+$/
+          if @current
+            set(@current, opt)
+          else
+            skip(opt)
+          end
+        when /^--([\w-]+)$/
+          keep($1)
+        when /^--([\w-]+)=([\w\s]+)$/
+          consume_current
+          set($1, $2)
+        when /^-(\w)(\w+)$/
+          consume_current
+          set($1, $2)
+        when /^-(\w)$/
+          consume_current
+          keep($1)
+        when nil
+          consume_current
         else
           skip(opt)
         end
-      when /^--([\w-]+)$/
-        keep($1)
-      when /^--([\w-]+)=([\w\s]+)$/
-        consume_current
-        set($1, $2)
-      when /^-(\w)(\w+)$/
-        consume_current
-        set($1, $2)
-      when /^-(\w)$/
-        consume_current
-        keep($1)
-      when nil
-        consume_current
-      else
-        skip(opt)
       end
     end
-  end
 
-  def sanitize(value)
-    return true   if value == "true"
-    return false  if value == "false"
-    return value.to_i if value =~ /^\d+$/
-    value
-  end
+    def sanitize(value)
+      return true   if value == "true"
+      return false  if value == "false"
+      return value.to_i if value =~ /^\d+$/
+      value
+    end
 
-  def set(key, value)
-    @options[key.to_sym] = sanitize(value)
-    @current = nil if @current
-  end
+    def set(key, value)
+      @options[key.to_sym] = sanitize(value)
+      @current = nil if @current
+    end
 
-  def keep(opt)
-    @current = opt
-  end
+    def keep(opt)
+      @current = opt
+    end
 
-  def skip(opt)
-    @skipped << opt
-  end
+    def skip(opt)
+      @skipped << opt
+    end
 
-  def consume_current
-    if @current
-      if @current =~ /^no-(.+)/
-        key   = $1
-        value = false
-      else
-        key   = @current
-        value = true
+    def consume_current
+      if @current
+        if @current =~ /^no-(.+)/
+          key   = $1
+          value = false
+        else
+          key   = @current
+          value = true
+        end
+        set(key, value)
       end
-      set(key, value)
     end
   end
 end
